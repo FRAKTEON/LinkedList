78f052b3c44d00461925b314a88ea0ac19ab0b89
ratanbratan <marekpazur1@seznam.cz>
2022-04-06 23:36:52 +0200

    -Added new functions

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ class Node {
         this.next = next
     }
 
-    insertAt(){
-    }
 }
 
 class List {
@@ -34,29 +32,78 @@ class List {
 
     pop() {
 
-        let x = this.head
+        let node = this.head
 
-        if(x==null)return
-        else if(x.next==null){this.head=null;return}
+        if(node===null)return null
+        else if(node.next==null){this.head=null;return null}
 
-        while (x.next.next !== null){
-            x = x.next
+        while (node.next.next !== null){
+            node = node.next
         }
 
-        x.next = null
+        node.next = null
     }
 
-    remove() {
+    clear(){
+    this.head = null
+    }
+
+    remove(element) {
+        let node = this.head
+        let removedNode = this.find(element)
+
+        if(node === removedNode){
+            this.head=node.next
+            return null
+        }
+
+        while(node.next !== removedNode){
+            node = node.next
+        }
+
+        node.next = node.next.next
     }
 
     reverse() {
+    }
+
+    insertAfter(element, value){
+        let node = this.head
+        let foundNode = this.find(element)
+
+        while(node !== foundNode){
+            node = node.next
+        }
+
+        let temp = node.next
+        node.next = new Node(value, temp)
 
     }
-    
-    find() {
+
+    find(element) {
+        let node = this.head
+        if(node==null)return null
+
+        while (node !== null) {
+            if(element ===node.value)return node
+            node = node.next
+        }
+
+        return null
     }
     
     print() {
+        let x = this.head
+        let length=this.getLength()
+
+        console.log("    Linked List    ")
+        console.log("-------------------")
+        for(let i=0; i < length; i++){
+            console.log("Node ("+i+"): "+x.value)
+            x=x.next
+        }
+        console.log("-------------------")
+        console.log("Number of nodes: "+length)
     }    
     
     sort() {
@@ -64,9 +111,8 @@ class List {
         let tempData
         const length=this.getLength()
         if(x==null)return
-        console.log(length)
 
-        for(let i = 0; i < length; i++)
+        for(let i = 0; i < length; i++){
             while (x.next !== null){
                 if(x.value > x.next.value){
                     tempData=x.value
@@ -75,6 +121,8 @@ class List {
                 }
                 x = x.next
             }
+        x = this.head
+        }
 
     }
 
@@ -93,9 +141,37 @@ class List {
         return listLength
     }
 
+    getHead(){
+        return this.head
+    }
+
+    getTail(){
+        let node = this.head
+        while(node.next !== null){
+            node=node.next
+        }
+        return node
+    }
+
 }
 
+function cmds(){
+    console.log("- list\n" +
+        "- print()\n" +
+        "- push(element)\n" +
+        "- insertAfter(element, value)\n" +
+        "- pop()\n" +
+        "- remove(element)\n" +
+        "- clear()\n" +
+        "- reverse()\n" +
+        "- sort()\n" +
+        "- find(element)\n" +
+        "- getLength\n" +
+        "- getHead\n" +
+        "- getTail")
+}
 const list = new List()
+console.log("Type cmds() for a list of commands.")
 
 
 
